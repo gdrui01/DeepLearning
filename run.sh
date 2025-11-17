2a2b8fa3bf2c67f31ab6163eaca2ac28fcf05fdf
momobhz <115977232+momobhz@users.noreply.github.com>
2025-11-17 09:41:58 +0100

    add run file for cluster

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# SBATCH --account=deep_learning
+# SBATCH --output=logs/mtbreaker_%j.out
+# SBATCH --time=10:00
+
+. /etc/profile.d/modules.sh
+module add cuda/12.1
+
+source "$HOME/venvs/mtbreaker/bin/activate"
+cd "$HOME/DeepLearning/"
+mkdir -p logs
+
+python -m src.RL_ppo_training \
+  --seeds data/seeds.txt \
+  --k 100 \
+  --steps 200 \
+  --batch_size 8
