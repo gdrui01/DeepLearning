8390844a862c9e9d28badcd16a6d2beb572367b3
Maurice Behanzin <mauricebehanzin@outlook.com>
2025-12-09 11:33:24 +0100

    modify checkpoint path on cluster

--- scripts/test_model.sh
+++ scripts/test_model.sh
@@ -15,4 +15,5 @@ mkdir -p logs
 
 # python -m src.scorers
 # python test_scripts/test_model_inference.py
-python test_scripts/test_model_inference.py --checkpoint "checkpoints/stage-1/" --base-model "Qwen/Qwen3-0.6B-Base"
+# "$HOME/ckpt/stage-x/"
+python test_scripts/test_model_inference.py --checkpoint "$HOME/ckpt/stage-x/" --base-model "Qwen/Qwen3-0.6B-Base"
